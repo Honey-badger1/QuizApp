d638c7a6408aad03def200b257629cd7fed860dd
Margo Astreyko <margoastreyko@mail.ru>
2020-08-27 23:36:49 +0300

    Updates

---
+++ precache-manifest.585201ec4da17bdfd886407acde6baf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471bd17fa7e808daa0c045475d3c3a0d",
+    "url": "/honey-badger1.github.io/QuizApp/index.html"
+  },
+  {
+    "revision": "77f60a7b6135251f4bb8",
+    "url": "/honey-badger1.github.io/QuizApp/static/css/main.df312494.chunk.css"
+  },
+  {
+    "revision": "c37fa906511f2ef10b85",
+    "url": "/honey-badger1.github.io/QuizApp/static/js/2.7c1741fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/honey-badger1.github.io/QuizApp/static/js/2.7c1741fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f60a7b6135251f4bb8",
+    "url": "/honey-badger1.github.io/QuizApp/static/js/main.b8bff1b9.chunk.js"
+  },
+  {
+    "revision": "e789b6cd8b22d6e08a8a",
+    "url": "/honey-badger1.github.io/QuizApp/static/js/runtime-main.c59184b2.js"
+  }
+]);
